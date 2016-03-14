57e5f886214b80cbcc538d138fa4db9ca036c9d8
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-03-14 13:52:00 +0100

    link controller

---
+++ app/Http/Controllers/LinkController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class LinkController extends Controller
+{
+    public function index()
+    {
+        $links = \App\Link::all();
+        return view('welcome', compact('links'));
+    }
+    public function show()
+    {
+        return view('submit');
+    }
+
+    public function create(Request $request)
+    {
+        //dd($request);
+        $validator = \Validator::make($request->all(), [
+            'title' => 'required|max:255',
+            'url' => 'required|max:255',
+            'description' => 'required|max:255'
+        ]);
+
+        if ($validator->fails()) {
+            return back()
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $link = new \App\Link;
+        $link->title = $request->title;
+        $link->url = $request->url;
+        $link->description = $request->description;
+        $link->save();
+
+        return redirect('/');
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,42 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
+
 
 Route::group(['middleware' => 'web'], function () {
-    Route::get('/', function () {
-        $links = \App\Link::all();
-        return view('welcome', compact('links'));
-    });
-
-    Route::get('/submit', function() {
-        return view('submit');
-    });
-
-    Route::post('/submit', function(Request $request) {
-        $validator = Validator::make($request->all(), [
-            'title' => 'required|max:255',
-            'url' => 'required|max:255',
-            'description' => 'required|max:255'
-        ]);
-
-        if ($validator->fails()) {
-            return back()
-                ->withInput()
-                ->withErrors($validator);
-        }
-
-        $link = new \App\Link;
-        $link->title = $request->title;
-        $link->url = $request->url;
-        $link->description = $request->description;
-        $link->save();
-
-        return redirect('/');
-    });
+    Route::get('/', 'LinkController@index');
 });
 
 Route::group(['middleware' => 'web'], function () {
     Route::auth();
 
     Route::get('/home', 'HomeController@index');
+
+    Route::get('/submit', 'LinkController@show');
+
+    Route::post('/submit', 'LinkController@create');
 });
